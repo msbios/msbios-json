d8b7b1618a0e7e007052a1302e6fbb4ef224072c
Judzhin Miles <info@msbios.com>
2019-01-10 16:14:07 +0200

    Lazy Composer MSBios Commit Script

---
+++ src/Module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\Json;
+
+/**
+ * Class Module
+ * @package MSBios\Json
+ */
+class Module
+{
+    const VERSION = '1.0.0';
+}
